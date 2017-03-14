7c8a9e9d35fcfbd3d91d2946d3ec33415e0b261a
atinaputri <putri.atina@gmail.com>
2017-03-14 10:07:54 +0700

    show pie chart

--- app/src/main/java/com/example/ciheul/baros/PersonnelStatistic.java
+++ app/src/main/java/com/example/ciheul/baros/PersonnelStatistic.java
@@ -1,15 +1,38 @@
 package com.example.ciheul.baros;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ciheul on 13/03/17.
  */
 
 public class PersonnelStatistic extends AppCompatActivity {
+
+    private RelativeLayout mainLayout;
+    private PieChart mChart;
+
+    private float[] yData = {5, 10, 15, 20, 25};
+    private String[] xData = {"Atina", "Avi", "Danu", "Ivan","Anggit"};
+    String desc = "Kinerja Keseluruhan";
 
     String sName = "";
     String sNrpRank = "";
@@ -19,6 +42,55 @@ public class PersonnelStatistic extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personnel_statistic);
+
+        /*PIE CHART*/
+        PieChart mChart = (PieChart)findViewById(R.id.piechart);
+
+        // configure pie chart
+        mChart.setUsePercentValues(true);
+
+        // enable rotation of the chart by touch
+        mChart.setRotationAngle(0);
+        mChart.setRotationEnabled(true);
+
+        // add data
+        // IMPORTANT: In a PieChart, no values (Entry) should have the same
+        // xIndex (even if from different DataSets), since no values can be
+        // drawn above each other.
+        ArrayList<Entry> yvalues = new ArrayList<Entry>();
+        yvalues.add(new Entry(8f, 0));
+        yvalues.add(new Entry(15f, 1));
+        yvalues.add(new Entry(12f, 2));
+        yvalues.add(new Entry(25f, 3));
+        yvalues.add(new Entry(23f, 4));
+        yvalues.add(new Entry(17f, 5));
+
+        PieDataSet dataSet = new PieDataSet(yvalues, "Election Results");
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        xVals.add("January");
+        xVals.add("February");
+        xVals.add("March");
+        xVals.add("April");
+        xVals.add("May");
+        xVals.add("June");
+
+        PieData data = new PieData(xVals, dataSet);
+
+        data.setValueFormatter(new PercentFormatter());
+        mChart.setDescription("");
+        mChart.setData(data);
+        mChart.setDrawHoleEnabled(true);
+        mChart.setTransparentCircleRadius(30f);
+        mChart.setHoleRadius(30f);
+        dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+
+        data.setValueTextSize(13f);
+        data.setValueTextColor(Color.DKGRAY);
+        mChart.animateXY(1400, 1400);
+
+        /*END OF PIE CHART*/
 
         getIntentData();
 
@@ -43,4 +115,5 @@ public class PersonnelStatistic extends AppCompatActivity {
         }
 
     }
+
 }
